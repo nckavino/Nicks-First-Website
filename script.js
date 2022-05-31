f4e89f9de9ebd40e68b508a56992ccb157a13fa4
Nicholas Avino <nckavino0903@gmail.com>
2022-05-30 22:51:25 -0400

    Added images folder and picture, linked to index, updated index, script, and style

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "This is something that I added using JavaScript, how cool is that?";
 
 document.querySelector("body").appendChild(h2);
